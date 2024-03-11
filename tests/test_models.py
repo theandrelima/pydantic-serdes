b852fe1f0dde675f82cee856262b3c41c6acd2a1
André Lima <39955130+theandrelima@users.noreply.github.com>
2024-03-11 21:26:26 +0000

    Automated tests workflow (#5)
    * added github workflow to run pytests
    
    * moved tests directory to project root and adjusted tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,41 +5,41 @@ import pytest
 # it's imported here. And because it's imported before we ever try to
 # create any model instance, the `MODELS_MODULES` env var doesn't need
 # to be set.
-from serial_bus.tests.models import CustomerModel
+from tests.models import CustomerModel
 from serial_bus import GLOBAL_DATA_STORE as data_store
 from serial_bus.utils import generate_from_file, load_file_to_dict
-from serial_bus.tests.utils import get_customer_model_records_for_assertion, get_customer_data_for_assertion
+from tests.utils import get_customer_model_records_for_assertion, get_customer_data_for_assertion
 
 
 ### FIXTURES ###
 @pytest.fixture
 def products_yaml_file():
-    return "serial_bus/tests/mocked_data/products.yaml"
+    return "tests/mocked_data/products.yaml"
 
 
 @pytest.fixture
 def customers_json_file():
-    return "serial_bus/tests/mocked_data/customers.json"
+    return "tests/mocked_data/customers.json"
 
 
 @pytest.fixture
 def customers_toml_file():
-    return "serial_bus/tests/mocked_data/customers.toml"
+    return "tests/mocked_data/customers.toml"
 
 
 @pytest.fixture
 def customers_yaml_file():
-    return "serial_bus/tests/mocked_data/customers.yaml"
+    return "tests/mocked_data/customers.yaml"
 
 
 @pytest.fixture
 def yml_customers_yaml_file():
-    return "serial_bus/tests/mocked_data/yml_customers.yaml"
+    return "tests/mocked_data/yml_customers.yaml"
 
 
 @pytest.fixture
 def rendered_customers_txt_file():
-    return "serial_bus/tests/mocked_data/rendered_customers.txt"
+    return "tests/mocked_data/rendered_customers.txt"
 
 
 @pytest.fixture
@@ -58,7 +58,7 @@ def test_generate_models(products_yaml_file, customers_json_file):
     # and this is enough to guarantee the 'directive_to_model_mapping' config
     # will be populated with the correct values. If that was not the case,
     # we'd have to set the 'MODELS_MODULES' env var as shown below:
-    # os.environ["MODELS_MODULES"] = "serial_bus.tests.models"
+    # os.environ["MODELS_MODULES"] = "tests.models"
 
     generate_from_file(products_yaml_file)
     generate_from_file(customers_json_file)
@@ -82,7 +82,7 @@ def test_ds_records_values(products_data, customers_data):
 
 
 def test_rendering(rendered_customers_txt_file):
-    os.environ["TEMPLATES_DIR"] = "serial_bus/tests/templates"
+    os.environ["TEMPLATES_DIR"] = "tests/templates"
 
     rendered_str = ''
 
