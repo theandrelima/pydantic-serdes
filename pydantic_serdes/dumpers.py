b7e8d3d82f52a6fa3a79a3f579cb8b3e7edfa398
André Lima <39955130+theandrelima@users.noreply.github.com>
2024-06-22 00:39:29 +0100

    code formatted with Black

--- pydantic_serdes/dumpers.py
+++ pydantic_serdes/dumpers.py
@@ -25,34 +25,27 @@ import yaml
 
 from pydantic_serdes.decorators import stream_dumper
 
+
 @stream_dumper
-def json_dumper(
-    data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs
-) -> str:
+def json_dumper(data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs) -> str:
     """Writes data to a JSON stream and optionally to a file."""
     json.dump(data, stream, *args, **kwargs)
 
 
 @stream_dumper
-def yaml_dumper(
-    data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs
-) -> str:
+def yaml_dumper(data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs) -> str:
     """Writes data to a YAML stream and optionally to a file."""
     yaml.dump(data, stream, *args, **kwargs)
 
 
 @stream_dumper
-def yml_dumper(
-    data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs
-) -> str:
+def yml_dumper(data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs) -> str:
     """Writes data to a YAML stream and optionally to a file."""
     yaml.dump(data, stream, *args, **kwargs)
 
 
 @stream_dumper
-def ini_dumper(
-    data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs
-) -> str:
+def ini_dumper(data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs) -> str:
     """Writes data to an INI stream and optionally to a file."""
     config = configparser.ConfigParser()
     config.read_dict(data)
@@ -60,8 +53,6 @@ def ini_dumper(
 
 
 @stream_dumper
-def toml_dumper(
-    data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs
-) -> str:
+def toml_dumper(data: Dict[Any, Any], stream: io.StringIO, *args, **kwargs) -> str:
     """Writes data to a TOML stream and optionally to a file."""
     toml.dump(data, stream, *args, **kwargs)
--- tests/models.py
+++ tests/models.py
@@ -1,12 +1,17 @@
 from pydantic import EmailStr, Field
 from pydantic_serdes import OneToMany
-from pydantic_serdes.models import PydanticSerdesBaseModel, PydanticSerdesRenderableModel
+from pydantic_serdes.models import (
+    PydanticSerdesBaseModel,
+    PydanticSerdesRenderableModel,
+)
 
 
 class ProductModel(PydanticSerdesBaseModel):
     _key = ("prod_id",)  # fields that should be used to uniquely ID a model instance
     _directive = "products"  # yaml key that indicates data for instances of this model
-    _err_on_duplicate = True  # if data leading to duplicates are present, should we error?
+    _err_on_duplicate = (
+        True  # if data leading to duplicates are present, should we error?
+    )
 
     prod_id: str
     name: str
