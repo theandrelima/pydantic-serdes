b806a9329f6a06ab3efa68675bc6f32136455144
André Lima <39955130+theandrelima@users.noreply.github.com>
2024-06-21 23:54:56 +0100

    updated tests to use OneToMany

--- pydantic_serdes/__init__.py
+++ pydantic_serdes/__init__.py
@@ -3,6 +3,7 @@ from pydantic_serdes.models import (
     PydanticSerdesBaseModel,
     PydanticSerdesRenderableModel,
 )
+from pydantic_serdes.custom_collections import OneToMany
 from pydantic_serdes.utils import HashableDict, HashableDictType
 
 GLOBAL_DATA_STORE = get_global_data_store()
@@ -11,6 +12,7 @@ GLOBAL_DATA_STORE = get_global_data_store()
 __all__ = [
     "PydanticSerdesBaseModel",
     "PydanticSerdesRenderableModel",
+    "OneToMany",
     "HashableDict",
     "HashableDictType",
     "GLOBAL_DATA_STORE",
